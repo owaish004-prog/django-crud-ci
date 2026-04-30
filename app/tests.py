14fd140e8cea61bbe8eebd6e492a0cbc3909cca0
mohdowaish <owaishsidd0786@gmail.com>
2026-05-01 00:13:41 +0530

    yml updated

--- app/tests.py
+++ app/tests.py
@@ -2,5 +2,5 @@ from django.test import TestCase
 from .models import Product
 class ProductTest(TestCase):
     def test_create(self):
-        p = Product.objects.create(name='Laptop', price=50000)
+        p = Product.objects.create(name='Lapto1p', price=5000)
         self.assertEqual(p.name, 'Laptop')
